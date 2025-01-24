c50dbdc36ff74465bf5562ab2dfa707f50739650
Piotr Kasprzyk <p.j.kasprzyk@gmail.com>
2025-01-24 19:28:24 +0100

    basic bubble animation

--- objects/bubble/bubble.gd
+++ objects/bubble/bubble.gd
@@ -2,22 +2,28 @@ extends Node2D
 
 
 var target: Vector2 = Vector2.ZERO
+var time = 0
+var speed = 100
+var t = 0.5 # influences moving towards target
+var p = 0.5 # influences oscillating
 
 @onready var collider: CollisionShape2D = $Collider
+@onready var animated_sprite: AnimationPlayer = $AnimationPlayer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animated_sprite.play("spawn")
 
 
 func set_target(new_target: Vector2) -> void:
 	target = new_target
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	pass
+	time += delta
+	var towards_target = (target - global_position).normalized()
+	var perpendicular = Vector2(towards_target.y, -towards_target.x)
+	global_position += (t * towards_target + p * perpendicular * sin(time)) * speed * delta
 
 
 func _on_input_event(viewport:Node, event:InputEvent, shape_idx:int) -> void:
