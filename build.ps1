0367a1475b750ad1d07c5da2a3b996fba1064db3
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-24 19:23:46 +0100

    cicd

---
+++ build.ps1
@@ -0,0 +1,2 @@
+Godot_v4.3-stable_win64.exe --export-debug "Android" "..\polyExport\GGJ2025.apk"
+C:\Users\s-zep\AppData\Local\Android\Sdk\platform-tools\adb install "C:\MyGamez\polyExport\GGJ2025.apk"
