f5657d15b01a8407430022facb5edc5d262ccde5
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-24 23:44:18 +0100

    pop bubbles

--- objects/bubble/bubble.gd
+++ objects/bubble/bubble.gd
@@ -10,6 +10,8 @@ var p = 0.5 # influences oscillating
 @onready var collider: CollisionShape2D = $Collider
 @onready var animated_sprite: AnimationPlayer = $AnimationPlayer
 
+static var pop_scene = load("res://objects/bubble/bubble_pop/bubble_pop.tscn")
+
 
 func _ready() -> void:
 	animated_sprite.play("spawn")
@@ -29,4 +31,12 @@ func _physics_process(delta: float) -> void:
 func _on_input_event(viewport:Node, event:InputEvent, shape_idx:int) -> void:
 	var touch = event as InputEventScreenTouch
 	if touch:
+		spawn_pop()
 		queue_free()
+		
+func spawn_pop():
+	var p = pop_scene.instantiate() as GPUParticles2D
+	get_parent().add_child(p)
+	p.global_position = global_position
+	p.emitting = true
+	p.finished.connect( func(): p.queue_free()) 
