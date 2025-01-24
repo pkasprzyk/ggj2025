17338f73a1fc75d85d405f70b41add353c0d505a
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-24 21:11:15 +0100

    spawner rework

--- objects/bubble_spawner/bubble_spawner.gd
+++ objects/bubble_spawner/bubble_spawner.gd
@@ -1,4 +1,6 @@
-extends Area2D
+@icon("res://objects/bubble/bubbles.svg")
+extends Node2D
+
 
 
 @export var bubble: PackedScene
@@ -6,21 +8,21 @@ extends Area2D
 
 
 @onready var spawn_timer: Timer = $SpawnTimer
-@onready var spawn_area: CollisionShape2D = $SpawnArea
-@onready var target = $Target
+@export var spawn_area_shape: CollisionShape2D
+@export var target : Node2D
+
+@export var rect_test : Rect2
 
 
 var active: bool = true
 var bubbles: int = 0
-var spawn_start: Vector2
-var spawn_end: Vector2
-
-func _ready() -> void:
-	spawn_start = spawn_area.shape.get_rect().position
-	spawn_end = spawn_area.shape.get_rect().end
 
 
 func _gen_random_pos():
+	var shape = spawn_area_shape.shape
+	rect_test = shape.get_rect()
+	var spawn_start = spawn_area_shape.global_position + shape.get_rect().position
+	var spawn_end = spawn_area_shape.global_position + shape.get_rect().end
 	var x = randf_range(spawn_start.x, spawn_end.x)
 	var y = randf_range(spawn_start.y, spawn_end.y)
 	return Vector2(x, y)
@@ -28,10 +30,10 @@ func _gen_random_pos():
 
 func _spawn_bubble() -> void:
 	var bubble_instance = bubble.instantiate()
-	bubble_instance.global_position = _gen_random_pos()
 	bubble_instance.set_target(target.global_position)
 	bubble_instance.connect("tree_exiting", _on_bubble_destroyed)
 	add_child(bubble_instance)
+	bubble_instance.global_position = _gen_random_pos()
 	bubbles += 1
 
 
