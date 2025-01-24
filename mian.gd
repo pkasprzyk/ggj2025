4711ceece1e2fe2256cb6ebbee04142baf364974
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-24 16:52:57 +0100

    bubble

---
+++ mian.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var bubble_scene = load("res://objects/bubble/bubble.tscn") as PackedScene 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.emulate_touch_from_mouse = true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _input(event):
+	var touch = event as InputEventScreenTouch
+	if touch:
+		var b = bubble_scene.instantiate()
+		b.position = touch.position
+		add_child(b)
+		 
