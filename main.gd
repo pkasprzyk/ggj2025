3f34fef4f0ff52b67db1998788427915f5e94265
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-24 20:41:30 +0100

    resolution settings fix

--- main.gd
+++ main.gd
@@ -4,10 +4,9 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.emulate_touch_from_mouse = true
-	GAME_STATE.init($Control/ScoreLabel)
+	GAME_STATE.init($HUD/ScoreLabel)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
