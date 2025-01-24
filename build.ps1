d082318e616408c78810a7a8c03dad815355bce8
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-24 23:00:33 +0100

    export web to itch

--- build.ps1
+++ build.ps1
@@ -4,3 +4,5 @@
 Start-Process -Wait Godot_v4.3-stable_win64.exe '--headless --export-debug "Android" "..\polyExport\GGJ2025.apk"'
 C:\Users\s-zep\AppData\Local\Android\Sdk\platform-tools\adb install "C:\MyGamez\polyExport\GGJ2025.apk"
 Start-Process -Wait Godot_v4.3-stable_win64.exe '--headless --export-debug "Web" "..\polyExport\web/index.html"'
+7z a  ..\polyExport\web.zip ..\polyExport\web\*
+butler.exe push ..\polyExport\web.zip namespacev/ggj2025:web
