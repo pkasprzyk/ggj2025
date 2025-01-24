f6d92f36655c1343fc92077abf51199046467fa8
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-24 20:16:37 +0100

    cicd fix

--- build.ps1
+++ build.ps1
@@ -1,2 +1,5 @@
-Godot_v4.3-stable_win64.exe --export-debug "Android" "..\polyExport\GGJ2025.apk"
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+# powershell.exe -File .\build.ps1
+
+Start-Process -Wait Godot_v4.3-stable_win64.exe '--headless --export-debug "Android" "..\polyExport\GGJ2025.apk"'
 C:\Users\s-zep\AppData\Local\Android\Sdk\platform-tools\adb install "C:\MyGamez\polyExport\GGJ2025.apk"
