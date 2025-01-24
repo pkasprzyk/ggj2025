36a3e285f26f69027d5246894a0895300cb8362a
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-24 20:25:38 +0100

    timer and game over

--- game_state.gd
+++ game_state.gd
@@ -5,6 +5,7 @@ extends Node
 
 
 @onready var score: int = 0
+var timer := 1.5 * 60.0
 
 var score_label: Label 
 
@@ -13,7 +14,12 @@ func init(label: Label) -> void:
 	print(label)
 	score_label = label
 
+func _process(delta: float) -> void:
+	timer -= delta
+	score_label.text = "Time %2d:%02d - Score: %s" % [int(timer)/60, int(timer)%60, score]
+	if timer < 0.0:
+		get_tree().paused = true
+		score_label.text += "\n GAME OVER"
 
 func increment_score(value: int) -> void:
 	score += value
-	score_label.text = "%s" % score
