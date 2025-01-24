e454c80cfbc82f96d6686df4006a8a4d80453fa0
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-24 22:30:20 +0100

    web export

--- build.ps1
+++ build.ps1
@@ -3,3 +3,4 @@
 
 Start-Process -Wait Godot_v4.3-stable_win64.exe '--headless --export-debug "Android" "..\polyExport\GGJ2025.apk"'
 C:\Users\s-zep\AppData\Local\Android\Sdk\platform-tools\adb install "C:\MyGamez\polyExport\GGJ2025.apk"
+Start-Process -Wait Godot_v4.3-stable_win64.exe '--headless --export-debug "Web" "..\polyExport\web/index.html"'
