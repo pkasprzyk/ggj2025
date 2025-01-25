3bad4bb1cf517a60ea57b66a3425c308acc5a60c
Lukasz Zephyr <s-zephyr@o2.pl>
2025-01-25 09:14:24 +0100

    HUD - add canvas group

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node
 func _ready() -> void:
 	Input.emulate_touch_from_mouse = true
 	GAME_STATE.init(
-		$HUD,
+		$HUDCanvasGroup/HUD,
 		$PlayerBases/PlayerLeftBase,
 		$PlayerBases/PlayerRightBase,
 		$BulletManager,
